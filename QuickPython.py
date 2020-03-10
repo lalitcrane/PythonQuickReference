8c8c9d1b7f7f61aebf5e03f1957d7599bebb6465
lalitcrane <48549060+lalitcrane@users.noreply.github.com>
2020-03-10 13:28:38 +0530

    Update QuickPython.py

--- QuickPython.py
+++ QuickPython.py
@@ -5,3 +5,17 @@
 import imp  #imp is a standard package in python
 imp.reload(MyPackage)
 
+or 
+
+import importlib
+importlib.reload(MyPackage)
+
+
+# Print statements
+foo = 'foo'
+bar = 'bar'
+
+foobar = '%s%s' % (foo, bar) # It is OK
+foobar = '{0}{1}'.format(foo, bar) # It is better
+foobar = '{foo}{bar}'.format(foo=foo, bar=bar) # It is best
+
